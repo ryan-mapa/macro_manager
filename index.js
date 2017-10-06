b51caca436d9762e6f990dc12967f089f737f68b
ryan mapa <ryan.m.mapa@gmail.com>
2017-10-06 11:03:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvasEl = document.getElementById("canvas");
+
+ const ctx = canvasEl.getContext("2d");
+ debugger;
+ ctx.fillStyle = 'blue';
+ ctx.fillRect(0, 0, 500, 500);
+ ctx.fill();
+});
